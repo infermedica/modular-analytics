6733bf38f0ad48820a1b8811ab51bb456b8eb347
Arkadiusz Krzymieński <arkadiusz.krzymienski@infermedica.com>
2022-04-15 15:02:38 +0200

    [WiP]test: add unit tests for analytics function

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,6 +9,29 @@ Object.keys(__analytics).forEach((singleModule) => {
   __analytics[singleModule].isEnabled = true;
 });
 
+const createMyModule = (support = [], name = 'mock') => {
+  const template = {
+    name,
+    trackView: jest.fn(),
+    trackEvent: jest.fn(),
+    trackConversion: jest.fn(),
+    initialize: () => new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(testObj);
+      }, 300);
+    }),
+  };
+  return Object.keys(template).reduce((module, method) => {
+    if (support.includes(method) || method === 'name') {
+      return {
+        ...module,
+        [method]: template[method],
+      };
+    }
+    return module;
+  }, {});
+};
+
 beforeEach(async () => {
   jest.resetModules();
   analyticsModules = await import('../src/modules');
@@ -22,65 +45,151 @@ beforeEach(async () => {
 });
 
 describe('analytics function', () => {
-  describe.each(['trackEvent', 'trackView'])('trackView and trackEvent function', (fn) => {
-    test(`${fn} - run all modules`, () => {
-      analytics.Analytics[fn](testStr, testObj, moduleNames);
-      analyticsModules.default.forEach((singleModule) => {
-        if (singleModule[fn]) {
-          expect(singleModule[fn]).toHaveBeenCalledWith(testStr, testObj);
-        }
-      });
-    });
-    test(`${fn} - run only passed modules`, () => {
-      analytics.Analytics[fn](testStr, testObj, ['debug', 'amplitude']);
-      analyticsModules.default.forEach((singleModule) => {
-        if (singleModule[fn]) {
-          const isCalled = singleModule.name !== 'googleTagManager' && singleModule.name !== 'infermedicaAnalytics';
-          // eslint-disable-next-line no-unused-expressions
-          isCalled
-            ? expect(singleModule[fn]).toHaveBeenCalledWith(testStr, testObj)
-            : expect(singleModule[fn]).not.toHaveBeenCalled();
-        }
-      });
-    });
-    test(`${fn} - with setGlobalProperties as string, run all modules`, () => {
-      analytics.Analytics.setGlobalProperties('global', 'asString');
-      analytics.Analytics[fn](testStr, testObj, moduleNames);
-      analyticsModules.default.forEach((singleModule) => {
-        if (singleModule[fn]) {
-          expect(singleModule[fn]).toHaveBeenCalledWith(testStr, { global: 'asString', ...testObj });
-        }
-      });
-    });
-    test(`${fn} - with setGlobalProperties as object, run all modules`, () => {
-      analytics.Analytics.setGlobalProperties({ global: 'asObject' });
-      analytics.Analytics[fn](testStr, testObj, moduleNames);
-      analyticsModules.default.forEach((singleModule) => {
-        if (singleModule[fn]) {
-          expect(singleModule[fn]).toHaveBeenCalledWith(testStr, { global: 'asObject', ...testObj });
-        }
-      });
+  test('track event is not called before response of initialize function', async (done) => {
+    let response;
+    const moduleMock = createMyModule(['initialize', 'trackEvent']);
+    setTimeout(() => {
+      expect(moduleMock.trackEvent).not.toHaveBeenCalled();
+      expect(response).toBe(undefined);
+      done();
+    }, 299);
+    response = await moduleMock.initialize();
+    moduleMock.trackEvent();
+  });
+  test('track event is called after response of initialize function', async (done) => {
+    let response;
+    const moduleMock = createMyModule(['initialize', 'trackEvent']);
+    setTimeout(() => {
+      expect(moduleMock.trackEvent).toHaveBeenCalled();
+      expect(response).toBe(testObj);
+      done();
+    }, 301);
+    response = await moduleMock.initialize();
+    moduleMock.trackEvent();
+  });
+
+  test('trackEvent - run all modules', () => {
+    analytics.Analytics.trackEvent(testStr, testObj, moduleNames);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackEvent) {
+        expect(singleModule.trackEvent).toHaveBeenCalledWith(testStr, testObj);
+      }
     });
   });
-  describe.each(['initialize', 'trackConversion'])('initialize and trackConversion function', (fn) => {
-    test(`${fn} - run all modules without params`, () => {
-      analytics.Analytics[fn]();
-      analyticsModules.default.forEach((singleModule) => {
-        if (singleModule[fn]) {
-          expect(singleModule[fn]).toHaveBeenCalled();
-        }
-      });
-    });
-    test(`${fn} - run all modules with params`, () => {
-      analytics.Analytics[fn](testObj);
-      analyticsModules.default.forEach((singleModule) => {
-        if (singleModule[fn]) {
-          expect(singleModule[fn]).toHaveBeenCalledWith(testObj);
-        }
-      });
+  test('trackEvent - run only passed modules', () => {
+    analytics.Analytics.trackEvent(testStr, testObj, ['debug', 'amplitude']);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackEvent) {
+        const isCalled = singleModule.name !== 'googleTagManager' && singleModule.name !== 'infermedicaAnalytics';
+        // eslint-disable-next-line
+        isCalled
+          ? expect(singleModule.trackEvent).toHaveBeenCalledWith(testStr, testObj)
+          : expect(singleModule.trackEvent).not.toHaveBeenCalled();
+      }
     });
   });
-  test('Vue Analytics - add $analytics to vue prototype', () => {
+  test('trackEvent - with global properties as string, run all modules', () => {
+    analytics.Analytics.setGlobalProperties('global', 'asString');
+    analytics.Analytics.trackEvent(testStr, testObj, moduleNames);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackEvent) {
+        expect(singleModule.trackEvent).toHaveBeenCalledWith(testStr, { global: 'asString', ...testObj });
+      }
+    });
+  });
+  test('trackEvent - with global properties as object, run all modules', () => {
+    analytics.Analytics.setGlobalProperties({ global: 'asObject' });
+    analytics.Analytics.trackEvent(testStr, testObj, moduleNames);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackEvent) {
+        expect(singleModule.trackEvent).toHaveBeenCalledWith(testStr, { global: 'asObject', ...testObj });
+      }
+    });
+  });
+
+  test('trackView - run all modules', () => {
+    analytics.Analytics.trackView(testStr, testObj, moduleNames);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackView) {
+        expect(singleModule.trackView).toHaveBeenCalledWith(testStr, testObj);
+      }
+    });
+  });
+  test('trackView - run only passed modules', () => {
+    analytics.Analytics.trackView(testStr, testObj, ['debug', 'amplitude']);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackView) {
+        const isCalled = singleModule.name !== 'googleTagManager' && singleModule.name !== 'infermedicaAnalytics';
+        // eslint-disable-next-line
+        isCalled
+          ? expect(singleModule.trackView).toHaveBeenCalledWith(testStr, testObj)
+          : expect(singleModule.trackView).not.toHaveBeenCalled();
+      }
+    });
+  });
+  test('trackView - with global properties as string, run all modules', () => {
+    analytics.Analytics.setGlobalProperties('global', 'asString');
+    analytics.Analytics.trackView(testStr, testObj, moduleNames);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackView) {
+        expect(singleModule.trackView).toHaveBeenCalledWith(testStr, { global: 'asString', ...testObj });
+      }
+    });
+  });
+  test('trackView - with global properties as object, run all modules', () => {
+    analytics.Analytics.setGlobalProperties({ global: 'asObject' });
+    analytics.Analytics.trackView(testStr, testObj, moduleNames);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackView) {
+        expect(singleModule.trackView).toHaveBeenCalledWith(testStr, { global: 'asObject', ...testObj });
+      }
+    });
+  });
+  test('setGlobalProperties - as string', () => {
+    // eslint-disable-next-line
+    const globalProperties = analytics.__get__('globalProperties');
+    analytics.Analytics.setGlobalProperties('test', 'test');
+    expect(globalProperties).toEqual(testObj);
+  });
+  test('setGlobalProperties - as object', () => {
+    // eslint-disable-next-line
+    const globalProperties = analytics.__get__('globalProperties');
+    analytics.Analytics.setGlobalProperties(testObj);
+    expect(globalProperties).toEqual(testObj);
+  });
+  test('initialize - run all modules without params', () => {
+    analytics.Analytics.initialize();
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.initialize) {
+        expect(singleModule.initialize).toHaveBeenCalled();
+      }
+    });
+  });
+  test('initialize - run all modules with params', () => {
+    analytics.Analytics.initialize(testObj);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.initialize) {
+        expect(singleModule.initialize).toHaveBeenCalledWith(testObj);
+      }
+    });
+  });
+  test('trackConversion - run all modules without params', () => {
+    analytics.Analytics.trackConversion();
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackConversion) {
+        expect(singleModule.trackConversion).toHaveBeenCalled();
+      }
+    });
+  });
+  test('trackConversion - run all modules with params', () => {
+    analytics.Analytics.trackConversion(testObj);
+    analyticsModules.default.forEach((singleModule) => {
+      if (singleModule.trackConversion) {
+        expect(singleModule.trackConversion).toHaveBeenCalledWith(testObj);
+      }
+    });
+  });
+  test('Vue Analytics - use as Plugin in Vue 2', () => {
     const vueInstance = { prototype: {} };
     analytics.VueAnalytics.install(vueInstance);
     expect(vueInstance.prototype.$analytics).toEqual(analytics.Analytics);
