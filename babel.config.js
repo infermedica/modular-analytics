4582d0a93b4003382eae0670e5ff64dc23d25621
Przemysław Spaczek <przemyslaw.spaczek@infermedica.com>
2022-04-21 13:05:56 +0200

    chore: lint

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
   env: {
     test: {
       plugins: ['babel-plugin-rewire'],
